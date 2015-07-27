a03d3fc7349141de0e1f8b08b50f53167128a14c
s7design <otrs@s7designcreative.com>
2015-07-27 10:16:37 +0200

    update FillTestSystem

--- FillTestsystem.pl
+++ FillTestsystem.pl
@@ -273,6 +273,8 @@ for my $Service ( @{ $Config->{Services} } ) {
 
     my $ServiceID = $CommonObject{ServiceObject}->ServiceAdd(
         %{$Service},
+        TypeID      => 11,
+        Criticality => '3 normal',
         ValidID => 1,
         UserID  => 1,
     );
@@ -319,6 +321,7 @@ for my $SLA ( @{ $Config->{SLAs} } ) {
 
     my $SLAID = $CommonObject{SLAObject}->SLAAdd(
         %{$SLA},
+        TypeID      => 11,
         ServiceIDs => \@ServiceIDs,
         ValidID    => 1,
         UserID     => 1,
