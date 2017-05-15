901f12ee6cd2cc709aad8a66e87ecabf6899ca78
s7design <otrs@s7designcreative.com>
2017-05-15 14:51:39 +0200

    cleaned up, removed old scripts

--- otrs_all_uninstall.sh
+++ otrs_all_uninstall.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# perl bin/otrs.Console.pl Admin::Package::Uninstall ../OTRS-packages/SaaSCockpit-5.0.11.opm
-# perl bin/otrs.Console.pl Admin::Package::Uninstall ../OTRS-packages/SaaSSOAP-5.0.3.opm
-perl bin/otrs.Console.pl Admin::Package::Uninstall ../OTRS-packages/OTRSDownloadStatsServer-5.0.3.opm
 perl ../scripts/Linker.pl -a uninstall -m /opt -o /opt/otrs -l All -d
