0efa4c29185de5ef730ba6881758bcd69cce059e
Milan Rakic <mrakic@s7designcreative.com>
2018-04-24 13:50:32 +0200

    Modify Branch param.

--- OTRS-AllInOne/Rebuild_Config.sh
+++ OTRS-AllInOne/Rebuild_Config.sh
@@ -53,7 +53,7 @@ else
 fi
 
 # Restart apache.
-if [[ $Branch != "master-internal"* ]]
+if [[ $Branch != *"master-internal"* ]]
 	then echo -e "\\n${yellow}Restart apache:"
 	echo -e "${green}"
 	sudo service apache2 restart
