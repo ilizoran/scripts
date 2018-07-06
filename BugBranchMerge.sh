83dff77f9bd366a781b30f16a15905732bd31e77
Sanjin Vik <svik@s7designcreative.com>
2018-07-06 15:42:23 +0200

    Added BugBranchMerge script.

---
+++ BugBranchMerge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+yellow=`tput setaf 3`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+BugBranch=$1
+
+if [[ ! $BugBranch ]]
+	then 
+		echo -e "Need bug fix branch!"
+	exit 0
+fi
+
+FrameworkBranch=$(git rev-parse --abbrev-ref HEAD)
+
+git checkout $BugBranch
+
+git merge $FrameworkBranch --commit -m "Merged $FrameworkBranch into bug fix branch $BugBranch"
+
+git checkout $FrameworkBranch
+
+git merge --squash --no-commit $BugBranch
