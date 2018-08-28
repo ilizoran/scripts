10039263afce11c71968801f31c0fe9986a5b900
Sanjin Vik <svik@s7designcreative.com>
2018-08-28 11:39:12 +0200

    Updated servers script.

--- startservers.sh
+++ startservers.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+Branch=$(git rev-parse --abbrev-ref HEAD)
+
 # Script should be executed from the OTRS folder and elastic search in /opt
-gnome-terminal \
-        --tab -e "perl $PWD/bin/otrs.Console.pl Dev::Tools::WebServer" \
-        --tab -e "/opt/elasticsearch/bin/elasticsearch" \
-        --tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar" \
+if [[ "$Branch" == "master" ]] 
+	then gnome-terminal \
+		--tab -e "perl /opt/otrs7-mojo/bin/otrs.Console.pl Dev::Tools::WebServer" \
+		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
+		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
+else 
+	gnome-terminal --tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
+fi
