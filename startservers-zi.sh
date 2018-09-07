db13b4c69d7fedb6f3336fc9c924c9d236a9d89d
Zoran Ilibasic <zilibasic@s7designcreative.com>
2018-09-07 15:32:05 +0200

    Added zi servers script.

--- startservers-zi.sh
+++ startservers-zi.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-cd /opt/otrs7
-Branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Script should be executed from the OTRS folder and elastic search in /opt
-if [[ "$Branch" == "master" ]] 
-	then gnome-terminal \
+cd /opt/otrs7
+
+gnome-terminal \
 		--tab -e "perl /opt/otrs7/bin/otrs.Console.pl Dev::Tools::WebServer" \
 		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
 		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
-else 
-	gnome-terminal --tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
-fi
