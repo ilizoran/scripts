44bb6de52e651c1ce757974fd918fe504c03c56c
Sanjin Vik <svik@s7designcreative.com>
2018-11-14 12:47:49 +0100

    Updated startservers script.

--- startservers.sh
+++ startservers.sh
@@ -3,7 +3,7 @@
 Branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Script should be executed from the OTRS folder and elastic search in /opt
-if [[ "$Branch" == *"master"* ]] 
+if [[ "$Branch" == *"rel-7_0"* ]] 
 	then gnome-terminal \
 		--tab -e "perl /opt/otrs7-mojo/bin/otrs.Console.pl Dev::Tools::WebServer" \
 		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
