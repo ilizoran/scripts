0c5613692aa8906b7f7480a7274f7988a086e0b6
Zoran Ilibasic <zilibasic@s7designcreative.com>
2019-05-31 15:05:14 +0200

    Updated scripts.

--- startservers-zi.sh
+++ startservers-zi.sh
@@ -4,6 +4,8 @@ gnome-terminal \
 		--tab -e "perl /opt/otrs7/bin/otrs.Console.pl Dev::Tools::WebServer" \
 		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
 		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar" \
-		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --port 3010" \
+		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app Agent --port 3010" \
+		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app External --port 3012" \
+
 
 
