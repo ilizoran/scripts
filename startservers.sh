8e51a1b91faafbd0d0b4b958243a47f619b1908b
Sanjin Vik <svik@s7designcreative.com>
2019-07-17 11:45:02 +0200

    Updated startservers.sh to support no elastic search.

--- startservers.sh
+++ startservers.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+NoElastic=$1
+
 Branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Script should be executed from the OTRS folder and elastic search in /opt
-if [[ "$Branch" == *"rel-7_0"* || "$Branch" == *"master"* ]]
+if [[ "$Branch" == *"rel-7_0"* ]] && [[ "$NoElastic" != "No" ]]
 	then gnome-terminal \
 		--tab -e "perl /opt/otrs7-mojo/bin/otrs.Console.pl Dev::Tools::WebServer --port 3000" \
+		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
+		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
+elif [[ "$Branch" == *"rel-7_0"* ]] && [[ "$NoElastic" == "No" ]]
+	then gnome-terminal \
+		--tab -e "perl /opt/otrs7-mojo/bin/otrs.Console.pl Dev::Tools::WebServer --port 3000" \
+		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
+elif [[ "$Branch" == *"master"* ]] && [[ "$NoElastic" != "No" ]]
+	then gnome-terminal \
 		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app Agent --port 3010" \
 		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app External --port 3012" \
 		--tab -e "/opt/elasticsearch/bin/elasticsearch" \
+		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
+elif [[ "$Branch" == *"master"* ]] && [[ "$NoElastic" != "No" ]]
+	then gnome-terminal \
+		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app Agent --port 3010" \
+		--tab -e "perl /opt/otrs8/bin/otrs.Console.pl Dev::Tools::WebServer --app External --port 3012" \
 		--tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
 else
 	gnome-terminal --tab -e "java -Dwebdriver.chrome.driver=/opt/scripts/chromedriver -jar /opt/scripts/selenium-server-standalone.jar"
